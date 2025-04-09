e34326124ac863f98d6ab6a87e425282d800b66f
Clea Parcerisas <clea.parcerisas@vliz.be>
2025-04-09 11:19:50 +0200

    added the overlay augmentation (and changed paths to match VLIZ servers)

--- train_cnn.py
+++ train_cnn.py
@@ -5,21 +5,21 @@ import numpy as np
 import torch
 import random
 import wandb
-from opensoundscape import CNN
+from opensoundscape import CNN, SpectrogramPreprocessor
 from opensoundscape.data_selection import resample
 from opensoundscape.preprocess.utils import show_tensor_grid
 from opensoundscape import AudioFileDataset
 from pathlib import Path
 from sklearn.metrics import roc_auc_score, f1_score, precision_score, recall_score, precision_recall_curve, auc
 from sklearn.model_selection import StratifiedKFold
-import tensorflow as tf #TF needed to import pickle
+import tensorflow as tf  # TF needed to import pickle
 import gc
 
+
 def create_train_valid_set(experiment_name, samples_dir, fish_sound, fish_sound_folder, window_len_s):
-    
     train_set_dir = os.path.join(samples_dir, experiment_name)
 
-    pos_samples_file = os.listdir(os.path.join(train_set_dir, fish_sound_folder)) 
+    pos_samples_file = os.listdir(os.path.join(train_set_dir, fish_sound_folder))
     neg_samples_file = os.listdir(os.path.join(train_set_dir, 'Unknown'))
 
     pos_samples_file = [os.path.join(train_set_dir, fish_sound_folder, file) for file in pos_samples_file]
@@ -48,13 +48,11 @@ def create_train_valid_set(experiment_name, samples_dir, fish_sound, fish_sound_
     df_trainset.to_csv('train_set_baseline.csv', index=False)
     print('train set saved in train_set_baseline.csv')
 
-    df_trainset = pd.read_csv('train_set_baseline.csv', index_col=[0,1,2])
+    df_trainset = pd.read_csv('train_set_baseline.csv', index_col=[0, 1, 2])
     return df_trainset
 
 
-
 def create_test_set_with_opensoundscape(test_files_path, fish_sound):
-    
     test_files_list = os.listdir(test_files_path)
 
     # Create a list of all wav files with files ending by .wav
@@ -70,7 +68,8 @@ def create_test_set_with_opensoundscape(test_files_path, fish_sound):
     selection_files = annot_files
     audio_files = wav_files
 
-    annotations = BoxedAnnotations.from_raven_files(raven_files=selection_files, audio_files=audio_files, annotation_column='Type')
+    annotations = BoxedAnnotations.from_raven_files(raven_files=selection_files, audio_files=audio_files,
+                                                    annotation_column='Type')
 
     clip_duration = 5.0
     clip_overlap = 0
@@ -82,52 +81,53 @@ def create_test_set_with_opensoundscape(test_files_path, fish_sound):
         clip_overlap=clip_overlap,
         min_label_overlap=min_label_overlap,
         class_subset=species_of_interest)
-    
-    return clip_labels
 
+    return clip_labels
 
 
 def load_test_set(test_set_path, test_files_path, fish_sound):
     # tensorflow is needed to import the pickle because it contains tensors
     test_set_df = pd.read_pickle(os.path.join(test_set_path, 'test_set.pkl'))
-    
+
     # Modify the format of the pickle to be compatible with opensoundscape
+    test_set_df[fish_sound] = (test_set_df['Label'] == fish_sound)
     test_set_df = test_set_df.drop(columns=['Label', 'Embedding'])
-    test_set_df = test_set_df.rename(columns={'label_int': fish_sound, 'Starttime': 'start_time', 'Endtime': 'end_time', 'filename': 'file'})
-    test_set_df[fish_sound] = test_set_df[fish_sound].astype(bool)
+    test_set_df = test_set_df.rename(columns={'Starttime': 'start_time', 'Endtime': 'end_time', 'filename': 'file'})
     test_set_df['file'] = test_files_path + test_set_df['file']
     test_set_df = test_set_df.set_index(['file', 'start_time', 'end_time'])
 
     return test_set_df
 
 
-
-def setup_model(model, sample_rate):
+def setup_model(model, sample_rate, labels):
     # all data augmentations
-    model.preprocessor.pipeline.random_trim_audio.bypass = True
 
-    model.preprocessor.pipeline.overlay.bypass = True
-    # model.preprocessor.pipeline.overlay.overlay_df = (train_df.astype(int))
-    # model.preprocessor.pipeline.overlay.set(overlay_class='A')
+    preprocessor = SpectrogramPreprocessor(2.0, overlay_df=labels)
 
-    model.preprocessor.pipeline.time_mask.bypass = True
-    model.preprocessor.pipeline.time_mask.set(max_masks=2, max_width=0.1)
+    # preprocessor.pipeline.random_trim_audio.bypass = True
 
-    model.preprocessor.pipeline.frequency_mask.bypass = True
-    model.preprocessor.pipeline.frequency_mask.set(max_masks=2, max_width=0.1)
+    preprocessor.pipeline.overlay.bypass = False
 
-    model.preprocessor.pipeline.add_noise.bypass = True
-    model.preprocessor.pipeline.random_affine.bypass = True
+    # preprocessor.pipeline.time_mask.bypass = True
+    preprocessor.pipeline.time_mask.set(max_masks=2, max_width=0.1)
 
-    model.preprocessor.pipeline.bandpass.set(min_f=50, max_f=2000)
+    # preprocessor.pipeline.frequency_mask.bypass = True
+    preprocessor.pipeline.frequency_mask.set(max_masks=2, max_width=0.1)
 
-    model.preprocessor.pipeline.to_spec.set(window_samples = 4 * (sample_rate // 100),
+    # preprocessor.pipeline.add_noise.bypass = True
+    # preprocessor.pipeline.random_affine.bypass = True
+
+    preprocessor.pipeline.bandpass.set(min_f=50, max_f=2000)
+
+    preprocessor.pipeline.to_spec.set(window_samples=4 * (sample_rate // 100),
                                             # overlap_samples = None,
                                             # fft_size = None,
                                             # dB_scale = True,
                                             # scaling = 'spectrum'
                                             )
 
+    model.preprocessor = preprocessor
+    print(model.preprocessor.pipeline)
 
 
 def compute_metrics(y_true, y_pred, fish_sound):
@@ -135,7 +135,7 @@ def compute_metrics(y_true, y_pred, fish_sound):
     true_labels = y_true[fish_sound].values
     pred_probs = y_pred[fish_sound].values
     pred_binary = pred_probs.round()  # Threshold = 0.5
-    
+
     # Calculate metrics
     precision = precision_score(true_labels, pred_binary, pos_label=1, average='binary')
     recall = recall_score(true_labels, pred_binary, pos_label=1, average='binary')
@@ -143,10 +143,9 @@ def compute_metrics(y_true, y_pred, fish_sound):
     auc_roc = roc_auc_score(true_labels, pred_probs)
     precision_curve, recall_curve, _ = precision_recall_curve(true_labels, pred_probs)
     auc_pr = auc(recall_curve, precision_curve)
-    
+
     # Return metrics as a dictionary for better access
     return precision, recall, f1, auc_roc, auc_pr
-
 
 
 if __name__ == "__main__":
@@ -157,14 +156,14 @@ if __name__ == "__main__":
 
     # Hard coded params for now - If properly given here, the rest of the code should run
     # TODO get them from the dict config eventually
-    fish_sound = 'A'
-    fish_sound_folder = 'fishA'
+    fish_sound = 'Jackhammer'
+    fish_sound_folder = 'Jackhammer'
     window_len_s = 5.0
     sample_rate = 32000
-    experiment_name = 'texel_baseline_20250331'
-    samples_dir = '/home/reindert/Valentin_REVO/surfperch_toshare/eval_texel Outputs/september 2024/surfperch/labeled_outputs/'
-    testset_files_dir = '/home/reindert/Valentin_REVO/surfperch_toshare/eval_texel Data/september 2024/test_set/'
-    testset_pickle_dir = '/home/reindert/Valentin_REVO/surfperch_toshare/eval_texel Outputs/september 2024/surfperch/test_set/'
+    experiment_name = 'jackhammer_surfperch_hockey_11_20250402'
+    samples_dir = '/mnt/fscompute_shared/agile_modeling/experiments_paper/output/grafton_deployment/surfperch/labeled_outputs/'
+    testset_files_dir = '/mnt/fscompute_shared/agile_modeling/experiments_paper/dataset/grafton_deployment/test_set/'
+    testset_pickle_dir = '/mnt/fscompute_shared/agile_modeling/experiments_paper/output/grafton_deployment/surfperch/test_set/'
 
     # WANDB
     activate_wandb = False
@@ -183,10 +182,10 @@ if __name__ == "__main__":
                 project="Agile Modeling and Opensoundscape",
                 name=wandb_exp_name,
             )
-        except:  
+        except:
             print("failed to create wandb session. wandb session will be None")
             wandb_session = None
-    else: 
+    else:
         wandb_session = None
 
     # NOT USED anymore - Create test set with opensoundscape - can be used to compare perf of both test set
@@ -203,9 +202,8 @@ if __name__ == "__main__":
 
     # Iterate over the folds
     for i, (train_index, valid_index) in enumerate(kf.split(df_trainset, df_trainset[fish_sound])):
-        
         print("\n\n----------------------------------------")
-        print(f"Running experiment for Fold {i+1}/5")
+        print(f"Running experiment for Fold {i + 1}/5")
         print("----------------------------------------")
 
         train_df, valid_df = df_trainset.iloc[train_index], df_trainset.iloc[valid_index]
@@ -215,14 +213,14 @@ if __name__ == "__main__":
 
         # We use Resnet as most common architeture used in Bioacoustics (ref: Stowell 2022)
         # Resnet 18 because how dataset is small (avoid overfitting)
-        architecture = 'resnet18' 
+        architecture = 'resnet18'
         class_list = [fish_sound]
         model = CNN(architecture=architecture,
                     classes=class_list,
                     sample_duration=5.0)
         # print(f"model.device is {model.device}")
 
-        setup_model(model, sample_rate)
+        setup_model(model, sample_rate, labels=train_df.sample(10))
 
         # TODO - Fix overlay
         # neg_train_df = train_df[train_df[fish_sound] == 0]
@@ -256,21 +254,23 @@ if __name__ == "__main__":
 
         # Run predictions on the validation set and on the test set
         predict_validset = model.predict(valid_df,
-                                        batch_size=batch_size, 
-                                        num_workers=num_workers, 
-                                        activation_layer='sigmoid', 
-                                        wandb_session=wandb_session,
-                                        progress_bar=False)
-        
-        pred_testset = model.predict(test_set_df, 
-                                     batch_size=batch_size, 
-                                     num_workers=num_workers, 
-                                     activation_layer='sigmoid', 
+                                         batch_size=batch_size,
+                                         num_workers=num_workers,
+                                         activation_layer='sigmoid',
+                                         wandb_session=wandb_session,
+                                         progress_bar=False)
+
+        pred_testset = model.predict(test_set_df,
+                                     batch_size=batch_size,
+                                     num_workers=num_workers,
+                                     activation_layer='sigmoid',
                                      wandb_session=wandb_session)
 
         # Compute metrics
-        prec_valid, recall_valid, f1_valid, auc_roc_valid, auc_pr_valid = compute_metrics(valid_df, predict_validset, fish_sound)
-        prec_test, recall_test, f1_test, auc_roc_test, auc_pr_test = compute_metrics(test_set_df, pred_testset, fish_sound)
+        prec_valid, recall_valid, f1_valid, auc_roc_valid, auc_pr_valid = compute_metrics(valid_df, predict_validset,
+                                                                                          fish_sound)
+        prec_test, recall_test, f1_test, auc_roc_test, auc_pr_test = compute_metrics(test_set_df, pred_testset,
+                                                                                     fish_sound)
 
         # Save metrics of the fold iteration
         validation_metrics_dic[i] = [prec_valid, recall_valid, f1_valid, auc_roc_valid, auc_pr_valid]
@@ -312,26 +312,25 @@ if __name__ == "__main__":
 
     # Save all metrics in a csv file
     metrics_df = pd.DataFrame({'fold': list(validation_metrics_dic.keys()),
-                            'precision_valid': [metrics[0] for metrics in validation_metrics_dic.values()],
-                            'recall_valid': [metrics[1] for metrics in validation_metrics_dic.values()],
-                            'f1_valid': [metrics[2] for metrics in validation_metrics_dic.values()],
-                            'auc_roc_valid': [metrics[3] for metrics in validation_metrics_dic.values()],
-                            'auc_pr_valid': [metrics[4] for metrics in validation_metrics_dic.values()],
-                            'precision_test': [metrics[0] for metrics in testset_metrics_dic.values()],
-                            'recall_test': [metrics[1] for metrics in testset_metrics_dic.values()],
-                            'f1_test': [metrics[2] for metrics in testset_metrics_dic.values()],
-                            'auc_roc_test': [metrics[3] for metrics in testset_metrics_dic.values()],
-                            'auc_pr_test': [metrics[4] for metrics in testset_metrics_dic.values()]})
-    
+                               'precision_valid': [metrics[0] for metrics in validation_metrics_dic.values()],
+                               'recall_valid': [metrics[1] for metrics in validation_metrics_dic.values()],
+                               'f1_valid': [metrics[2] for metrics in validation_metrics_dic.values()],
+                               'auc_roc_valid': [metrics[3] for metrics in validation_metrics_dic.values()],
+                               'auc_pr_valid': [metrics[4] for metrics in validation_metrics_dic.values()],
+                               'precision_test': [metrics[0] for metrics in testset_metrics_dic.values()],
+                               'recall_test': [metrics[1] for metrics in testset_metrics_dic.values()],
+                               'f1_test': [metrics[2] for metrics in testset_metrics_dic.values()],
+                               'auc_roc_test': [metrics[3] for metrics in testset_metrics_dic.values()],
+                               'auc_pr_test': [metrics[4] for metrics in testset_metrics_dic.values()]})
+
     # create results dir
     Path("./results").mkdir(parents=True, exist_ok=True)
     metrics_df.to_csv(f"./results/{experiment_name}_metrics.csv", index=False)
     print(f"Results saved in ./results/{experiment_name}_metrics.csv")
-    
+
     # Finish the session before next run
     if activate_wandb:
         wandb.finish()
-
 
 # Perf on last run
 '''
